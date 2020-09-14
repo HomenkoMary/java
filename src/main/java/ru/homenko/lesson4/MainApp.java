c150af67e4b494b77ed3c3e02c985cc98212344a
HomenkoMary <61244966+HomenkoMary@users.noreply.github.com>
2020-09-14 22:57:20 +0400

    lesson4

---
+++ src/main/java/ru/homenko/lesson4/MainApp.java
@@ -0,0 +1,122 @@
+package ru.homenko.lesson4;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MainApp {
+    public static int SIZE = 3;
+    public static int DOTS_TO_WIN = 3;
+    public static final String DOT_EMPTY = "•";
+    public static final String DOT_X = "X";
+    public static final String DOT_O = "O";
+    public static String[][] map = new String[SIZE][SIZE];
+    public static Scanner sc = new Scanner(System.in);
+    public static Random rand = new Random();
+
+    public static void main(String[] args) {
+        initMap();
+        printMap();
+        while (true) {
+            humanTurn();
+            printMap();
+            if (checkWin(DOT_X)) {
+                System.out.println("Победил человек");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+            aiTurn();
+            printMap();
+            if (checkWin(DOT_O)) {
+                System.out.println("Победил Искуственный Интеллект");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+        }
+        System.out.println("Игра закончена");
+    }
+
+    public static boolean checkLine(int start_x, int start_y, int dx, int dy, String sign) {
+        for (int i = 0; i < 3; i++)
+        {
+            if (map[start_x + i * dx][start_y + i * dy] != sign)
+                return false;
+        }
+        return true;
+    }
+
+    public static boolean checkWin(String sign) {
+        for (int i = 0; i < 2; i++)
+        {
+            if (checkLine(i, 0, 0, 1, sign)) return true;
+            if (checkLine(0, i, 1, 0, sign)) return true;
+        }
+
+        if (checkLine(0, 0, 1, 1, sign)) return true;
+        if (checkLine(0, 2 - 1, 1, -1, sign)) return true;
+        return false;
+    }
+
+    public static boolean isMapFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY) return false;
+            }
+        }
+        return true;
+    }
+
+    public static void aiTurn() {
+        int x, y;
+        do {
+            x = rand.nextInt(SIZE);
+            y = rand.nextInt(SIZE);
+        } while (!isCellValid(x, y));
+        System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y + 1));
+        map[y][x] = DOT_O;
+    }
+
+    public static void humanTurn() {
+        int x, y;
+        do {
+            System.out.println("Введите координаты в формате X Y");
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+        } while (!isCellValid(x, y)); // while(isCellValid(x, y) == false)
+        map[y][x] = DOT_X;
+    }
+
+    public static boolean isCellValid(int x, int y) {
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
+        if (map[y][x] == DOT_EMPTY) return true;
+        return false;
+    }
+
+    public static void initMap() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOT_EMPTY;
+            }
+        }
+    }
+
+    public static void printMap() {
+        for (int i = 0; i <= SIZE; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
