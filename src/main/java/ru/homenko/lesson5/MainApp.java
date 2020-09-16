6d523a4e2a7af526e074a38c199bcd46c4969e9b
HomenkoMary <61244966+HomenkoMary@users.noreply.github.com>
2020-09-16 23:53:53 +0400

    lesson 5 clean code

--- src/main/java/ru/homenko/lesson5/MainApp.java
+++ src/main/java/ru/homenko/lesson5/MainApp.java
@@ -3,12 +3,8 @@ package ru.homenko.lesson5;
 import sun.plugin.javascript.navig.Array;
 
 public class MainApp {
-
-
     public static void main(String[] args)
     {
-
-
         Employer[] Employee = new Employer[5];
 
         Employee[0] = new Employer("Anton", "Khomenko", 41, "xoma99@mail.ru", "+79179525900", "scrum master", 105000);
@@ -17,18 +13,10 @@ public class MainApp {
         Employee[3] = new Employer("Sidor", "Sidorov", 29, "sidor@mail.ru", "+79179525903", "project manager", 105000);
         Employee[4] = new Employer("Mike", "Dudikof", 29, "mike@mail.ru", "+79179525904", "actor", 805000);
 
-
-
-
         for (int i = 0; i < Employee.length; i++) {
             if (Employee[i].getEmployerAge() > 40){
                 System.out.println(Employee[i].getEmployerInfo());
             }
         }
     }
-
-
-
-
-
 }
