7d46e0668adc485a8cedbf6ffaa623797f1f60c5
HomenkoMary <61244966+HomenkoMary@users.noreply.github.com>
2020-09-18 00:40:42 +0400

    lesson6

---
+++ src/main/java/ru/homenko/lesson6/Animal.java
@@ -0,0 +1,8 @@
+package ru.homenko.lesson6;
+
+public abstract class Animal {
+    boolean result;
+    public abstract void run(int distance);
+    public abstract void swim(int distance);
+    public abstract void jump(double height);
+}
